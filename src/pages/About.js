e08aba1a548cf2d7196df96340790b1f36485544
Joel <joellucasandedi@gmail.com>
2023-02-28 03:05:51 +0100

    finished-project

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A PROPOS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit placeat tempora repellendus accusamus fuga ducimus blanditiis ullam assumenda dolorem consequuntur odit maxime minima inventore odio rem minus, tenetur nulla dignissimos amet iure quis earum suscipit laudantium. Esse maiores nobis, impedit, aliquam dolorum non perspiciatis id illum sed ipsam inventore aut nulla doloribus explicabo quibusdam cum tempora nesciunt natus velit vel? Enim doloribus dicta adipisci voluptatem nihil non ipsum dolorem provident iure reprehenderit ipsa dolore, aut similique sapiente fugiat ab repudiandae nobis vitae nesciunt porro iste! Deleniti magni voluptatem vero ut inventore similique, cupiditate maiores, repellendus, dolorum qui quas cumque in.
+
+            </p>
+            <br />
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, doloribus. Ratione facilis accusamus quaerat quo vitae tempore itaque aperiam quod fugiat ad, laudantium natus beatae excepturi, molestias cupiditate non blanditiis et iure aliquid quam, soluta hic dolores enim commodi? Quisquam, enim atque illum alias itaque repudiandae tenetur, tempora earum esse eos facilis. Fugiat odio rem, quod sunt dolorem tempore corrupti cumque consequuntur ducimus culpa! Cumque dolorem fugit obcaecati vitae quidem voluptatibus nisi ut modi hic qui! Dolor, ullam vero provident at harum suscipit eveniet beatae, impedit iusto cumque nam nulla temporibus voluptatibus. Corporis iste dolore quos obcaecati, mollitia sed consectetur cum doloribus nulla exercitationem illo? Rerum aliquid laboriosam quasi delectus minima in quam ratione unde repellat asperiores quo, tenetur at consequatur quod iure eius quibusdam fuga suscipit, facere nostrum quae, amet incidunt. Temporibus amet assumenda ab ipsum aperiam illo. Quas ratione repellat unde nisi optio explicabo praesentium molestias possimus culpa eos architecto, cupiditate nulla ipsa odio. Facere animi aperiam tempore libero praesentium et, fugit aspernatur cum! Commodi assumenda nemo quia adipisci nisi ipsa distinctio doloremque iusto libero? Numquam perferendis in odit minima voluptas quam rerum, inventore commodi sed facilis voluptatum, ratione deserunt tempora eum, incidunt ab cum dignissimos ullam quia?</p>
+        </div>
+    );
+};
+
+export default About;
